c15ac314194e8f7923c27a658c77026f926c3663
ramabhadrarao <ramabhadrarao.maddu@yahoo.com>
2025-05-09 09:11:58 +0530

    commit

--- app.py
+++ app.py
@@ -161,4 +161,4 @@ def _create_initial_roles():
 
 if __name__ == '__main__':
     app = create_app(os.getenv('FLASK_ENV', 'development'))
-    app.run(debug=True)  # Added app.run() for convenience
+    app.run(debug=True, host='0.0.0.0', port=5000)  # Added app.run() for convenience
